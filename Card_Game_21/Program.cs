be5c2a7c0ae4bcc12754e89f5be78f0c72426b99
prokhor_kostja <prokhor.kostja@gmail.com>
2019-05-19 16:56:18 +0300

    Create Structs and Enums

--- Card_Game_21/Program.cs
+++ Card_Game_21/Program.cs
@@ -6,6 +6,28 @@ using System.Threading.Tasks;
 
 namespace Card_Game_21
 {
+    enum Suit
+    {
+        Hurt,
+        Diamond,
+        Spades,
+        Clubs
+    }
+
+    enum Face
+    {
+        Jack, Qween, King,
+        Six, Seven, Eight,
+        Nine, Ten, Ace
+    }
+
+    struct Card
+    {
+        public Suit Suit;
+        public Face Face;
+        public int cardPoint;
+    }
+
     class Program
     {
         static void Main(string[] args)
