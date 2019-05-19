c8166331001060c892e87743f2d2bc1456ec44dc
prokhor_kostja <prokhor.kostja@gmail.com>
2019-05-19 16:57:39 +0300

    Generate card deck

--- Card_Game_21/Program.cs
+++ Card_Game_21/Program.cs
@@ -32,6 +32,29 @@ namespace Card_Game_21
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+
+            int suitLength = Enum.GetNames(typeof(Suit)).Length;
+            int faceLenght = Enum.GetNames(typeof(Face)).Length;
+
+            Card[] deckOfCards = new Card[suitLength * faceLenght];
+            int[] facePoint = new int[deckOfCards.Length];
+
+
+            for (int y = 0; y < faceLenght; y++)
+            {
+                facePoint[(int)Face.Jack] = 2;
+                facePoint[(int)Face.Qween] = 3;
+                facePoint[(int)Face.King] = 4;
+                facePoint[(int)Face.Six] = 6;
+                facePoint[(int)Face.Seven] = 7;
+                facePoint[(int)Face.Eight] = 8;
+                facePoint[(int)Face.Nine] = 9;
+                facePoint[(int)Face.Ten] = 10;
+                facePoint[(int)Face.Ace] = 11;
+            }
+
+
         }
     }
 }
